4d4aad976349d7b76393775056be7c1d4ae15cf8
jpwakugawa <jp_wakugawa@outlook.com>
2022-10-08 20:26:30 -0400

    array with lucky number

--- Coroutines/sequencial.lua
+++ Coroutines/sequencial.lua
@@ -1,19 +1,28 @@
 array = {}
 
-for i=1,5 do
+n = 10
+for i=1,n do
 	array[i] = {}
 
-	for j=1,5 do
-		array[i][j] = math.random(10)
+	for j=1,n do
+		random = math.random(n)
+		if (i==1)
+			then
+			array[i][j] = random
+		else
+			prev = array[i-1][random]
+			array[i][j] = prev
+		end
 	end
 end
 
 function printArray()
-	for i=1,5 do
-		for j=1,5 do
-			result = array[i][j]
-			print(result)
+	for i=1,n do
+		line = ""
+		for j=1,n do
+			line = line .. " " .. tostring(array[i][j])
 		end
+		print(line)
 	end
 end
 
