283def7ff76707ba6218d89ab2535df9c151e9e1
jpwakugawa <jp_wakugawa@outlook.com>
2022-10-13 17:00:47 -0400

    add threads in loop

---
+++ Threads/tutorial6.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int mails = 0;
+pthread_mutex_t mutex;
+
+// Mutex
+void* routine() {
+	for(int i=0; i<10000; i++) {
+		pthread_mutex_lock(&mutex);
+		mails++;
+		pthread_mutex_unlock(&mutex);
+	}
+	// read 
+	// increment
+	// write
+}
+
+int main(int argc, char* argv[]) {
+  pthread_t th[4];
+  pthread_mutex_init(&mutex, NULL);
+
+  for(int i=0; i<4; i++) {
+	  if (pthread_create(th + i, NULL, &routine, NULL) != 0) {
+	    perror("Failed to create thread");
+	    return 1;
+	  }
+	  printf("Thread %d has started!\n", i);
+  }
+
+
+  for(int i=0; i<4; i++) {
+	  if (pthread_join(th[i], NULL) != 0) {
+	    return 2;
+	  }
+	  printf("Thread %d has finished!\n", i);
+  }
+
+ 
+  pthread_mutex_destroy(&mutex);
+  printf("Number of mails: %d\n", mails);
+
+  return 0;
+}
