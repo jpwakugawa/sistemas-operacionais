9198fee38957507da61432bc6e7648059d8bd1e4
jpwakugawa <jp_wakugawa@outlook.com>
2022-10-17 16:39:20 -0400

    add clock_t && argc

--- Lucky Number/sequential.c
+++ Lucky Number/sequential.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-int n = 10;
-
-int main() {
-	time_t begin = time(NULL);
-
+int main(int argc, char *argv[]) {
 	srand(time(NULL));
+	clock_t begin = clock();
+
+	int n = atoi(argv[1]);
 	int X[n][n];
 
 	for(int i=0; i<n; i++) {
@@ -32,7 +31,7 @@ int main() {
 
 	}
 
-	time_t end = time(NULL);
+	clock_t end = clock();
 	double exec_time = end - begin;
 	printf("Execution Time: %f\n", exec_time);
 	return 0;
