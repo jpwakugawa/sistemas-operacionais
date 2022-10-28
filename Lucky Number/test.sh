f6542eee67bf955c1fc7ff72771a3ab2734a9622
jpwakugawa <jp_wakugawa@outlook.com>
2022-10-27 23:44:47 -0400

    add semaphore.c to testing script

--- Lucky Number/test.sh
+++ Lucky Number/test.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-gcc -g sequential.c -o sequential
-gcc -g -pthread n_threads.c -o n_threads
+gcc -g sequential.c -o sequential.e
+gcc -g -pthread n_threads.c -o n_threads.e
+# gcc -g -pthread semaphore.c -o semaphore.e
 
 for n in 10 100 1000
 do
-  echo
-  echo "TESTS WITH N = $n"
-  echo "=== Sequential ==="
-  time ./sequential $n 
+  echo "===================="
+  echo "Tests with n = $n"
+  echo "=== Sequential ====="
+  time ./sequential.e $n 
 
-  echo "=== N_Threads ==="
-  time ./n_threads $n
+  echo "=== N_Threads ======"
+  time ./n_threads.e $n
   echo
+
+  # echo "=== Semaphore ======"
+  # time ./semaphore.e $n
+  # echo
 done
